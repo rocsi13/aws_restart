77a1471db5eca0ae4c639701d17c6beb9ccac114
rocsi13 <103496696+rocsi13@users.noreply.github.com>
2024-02-22 20:06:11 +0200

    Add files via upload

---
+++ reStart-python-cloud9/environment/exercises.py
@@ -0,0 +1,16 @@
+print("Roxi")
+
+print("Midnight call me up")
+
+print(1,2,3,4,5,6)
+
+print(64+32)
+
+a=(64, 32)
+print(sum(a, 0))
+
+from datetime import date
+today=date.today()
+day=today.strftime("%d %m %Y")
+print("today is ", day)
+
